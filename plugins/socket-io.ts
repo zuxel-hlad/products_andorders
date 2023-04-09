b23f5415a8585770d58be4f409351daf935955a0
Stanislav Shchelokov <stanislavschelokov@gmail.com>
2023-04-09 20:54:16 +0300

    some commit

--- plugins/socket-io.ts
+++ plugins/socket-io.ts
@@ -1,9 +1,9 @@
 import io from 'socket.io-client';
 import { useStore } from '~/store';
-console.log('PROCESS ENVV!!!!!!',process.env);
+
 export default defineNuxtPlugin(({ $pinia }: any) => {
     const { setActiveSessions } = useStore($pinia);
-    const socket = io(`http://localhost:${process?.env?.PORT || 5000}`);
+    const socket = io('https://orders-products.netlify.app');
     socket.on('sessions', (sessions) => {
         setActiveSessions(sessions);
     });
