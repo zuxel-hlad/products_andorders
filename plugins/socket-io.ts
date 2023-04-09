5ece59dd90ea43aaf08bb3271e8c8514196f530c
Stanislav Shchelokov <stanislavschelokov@gmail.com>
2023-04-09 23:48:23 +0300

    added port

--- plugins/socket-io.ts
+++ plugins/socket-io.ts
@@ -1,11 +1,10 @@
 import io from 'socket.io-client';
 import { useStore } from '~/store';
-// import { workport } from '~/server';
-// console.log('WORKPORT', workport);
-const port = process?.env?.PORT || 5000;
+
 export default defineNuxtPlugin(({ $pinia }: any) => {
-    console.log('port');
     const { setActiveSessions } = useStore($pinia);
+    console.log(process.env.NODE_ENV);
+    const port = process.env.NODE_ENV === 'development' ? 5000 : 3000;
     const socket = io(`http://localhost:${port}`);
     socket.on('sessions', (sessions) => {
         setActiveSessions(sessions);
