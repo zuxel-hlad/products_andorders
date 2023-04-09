1ae5e3a80a676e0c19d63c4e95152231854e6ad7
Stanislav Shchelokov <stanislavschelokov@gmail.com>
2023-04-09 21:03:44 +0300

    added port

--- plugins/socket-io.ts
+++ plugins/socket-io.ts
@@ -1,9 +1,9 @@
 import io from 'socket.io-client';
 import { useStore } from '~/store';
-
+console.log(window.location.port);
 export default defineNuxtPlugin(({ $pinia }: any) => {
     const { setActiveSessions } = useStore($pinia);
-    const socket = io('https://unique-kitten-487a3d.netlify.app');
+    const socket = io('http://localhost:5000');
     socket.on('sessions', (sessions) => {
         setActiveSessions(sessions);
     });
