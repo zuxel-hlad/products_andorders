df28e04f22831d300cbdb011e1d538d5407ae215
Stanislav Shchelokov <stanislavschelokov@gmail.com>
2023-04-09 18:47:42 +0300

    added new port

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 5000;
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
