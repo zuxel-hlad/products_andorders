e10e74506e348520d7984a57c9360fb2b7229eab
Stanislav Shchelokov <stanislavschelokov@gmail.com>
2023-04-09 20:47:55 +0300

    try to run server

--- plugins/socket-io.ts
+++ plugins/socket-io.ts
@@ -1,9 +1,9 @@
 import io from 'socket.io-client';
 import { useStore } from '~/store';
-
+console.log('PROCESS ENVV!!!!!!',process.env);
 export default defineNuxtPlugin(({ $pinia }: any) => {
     const { setActiveSessions } = useStore($pinia);
-    const socket = io('http://localhost:5000' || process.env.PORT);
+    const socket = io(`http://localhost:${process?.env?.PORT || 5000}`);
     socket.on('sessions', (sessions) => {
         setActiveSessions(sessions);
     });
