c97a8eb58148a1cc6500d26a0f9868d935327eb7
Stanislav Shchelokov <stanislavschelokov@gmail.com>
2023-04-09 20:29:20 +0300

    try to run users counter

--- plugins/socket-io.ts
+++ plugins/socket-io.ts
@@ -3,7 +3,7 @@ import { useStore } from '~/store';
 
 export default defineNuxtPlugin(({ $pinia }: any) => {
     const { setActiveSessions } = useStore($pinia);
-    const socket = io('http://localhost:5000');
+    const socket = io('http://localhost:5000' || process.env.PORT);
     socket.on('sessions', (sessions) => {
         setActiveSessions(sessions);
     });
