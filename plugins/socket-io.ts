3e4488bf86a8b60e16bf16bfa3dd3b54fb2fd2f6
Stanislav Shchelokov <stanislavschelokov@gmail.com>
2023-04-09 21:08:57 +0300

    added some fixes

--- plugins/socket-io.ts
+++ plugins/socket-io.ts
@@ -3,7 +3,7 @@ import { useStore } from '~/store';
 console.log(window.location.port);
 export default defineNuxtPlugin(({ $pinia }: any) => {
     const { setActiveSessions } = useStore($pinia);
-    const socket = io('http://localhost:5000');
+    const socket = io('http://localhost:5000' || window.location.origin);
     socket.on('sessions', (sessions) => {
         setActiveSessions(sessions);
     });
