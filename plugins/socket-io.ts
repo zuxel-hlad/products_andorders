7b62a3157c9517de527a8729617af856030ba446
Stanislav Shchelokov <stanislavschelokov@gmail.com>
2023-04-10 04:15:01 +0300

    updated project

--- plugins/socket-io.ts
+++ plugins/socket-io.ts
@@ -3,10 +3,7 @@ import { useStore } from '~/store';
 
 export default defineNuxtPlugin(({ $pinia }: any) => {
     const { setActiveSessions } = useStore($pinia);
-    const url = 'http://localhost:5000';
-    // process.env.NODE_ENV === 'development'
-    //     ? 'http://127.0.0.1:5173/sessions'
-    //     : 'https://orders-products.netlify.app';
+    const url = 'https://sessions-count.netlify.app';
     const socket = io(`${url}`);
     socket.on('sessions', (sessions) => {
         setActiveSessions(sessions);
