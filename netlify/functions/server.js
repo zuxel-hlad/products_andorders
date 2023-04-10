44e0065735d6ade8d42f8d7958c85ca04e6695f0
Stanislav Shchelokov <stanislavschelokov@gmail.com>
2023-04-10 15:57:15 +0300

    added files

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,37 +1,35 @@
-const port = 4000;
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server, {
-    cors: {
-        origin: ['http://localhost:3000', 'https://orders-products.netlify.app', 'http://localhost:8888'],
-        methods: ['GET', 'POST'],
-        allowedHeaders: ['my-custom-header'],
-        credentials: true,
-    },
-});
-
-let sessions = 0;
-
-io.on('connection', (socket) => {
-    sessions++;
-    io.emit('sessions', sessions);
-
-    socket.on('disconnect', () => {
-        sessions--;
-        io.emit('sessions', sessions);
+exports.handler = async (event, context) => {
+    const port = 4000;
+    const express = require('express');
+    const app = express();
+    const server = require('http').createServer(app);
+    const io = require('socket.io')(server, {
+        cors: {
+            origin: ['http://localhost:3000', 'https://orders-products.netlify.app', 'http://localhost:8888'],
+            methods: ['GET', 'POST'],
+            allowedHeaders: ['my-custom-header'],
+            credentials: true,
+        },
     });
-});
 
-server.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+    let sessions = 0;
 
-export const handler = async () => {
+    io.on('connection', (socket) => {
+        sessions++;
+        io.emit('sessions', sessions);
+
+        socket.on('disconnect', () => {
+            sessions--;
+            io.emit('sessions', sessions);
+        });
+    });
+
+    server.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+
     return {
         statusCode: 200,
-        body: JSON.stringify({
-            data: sessions,
-        }),
+        body: JSON.stringify({ sessions: sessions }),
     };
 };
--- plugins/socket-io.ts
+++ plugins/socket-io.ts
@@ -3,7 +3,9 @@ import { useStore } from '~/store';
 
 export default defineNuxtPlugin(({ $pinia }: any) => {
     const { setActiveSessions } = useStore($pinia);
-    fetch('/.netlify/functions/server').then((response) => response.json());
+    fetch('/.netlify/functions/server')
+        .then((response) => response.json())
+        .then((data) => console.log(data));
     const url = 'http://localhost:4000';
     const socket = io(`${url}`);
     socket.on('sessions', (sessions) => {
